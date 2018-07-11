74f83d1c7ceec10bac4289cc10edccd06464bb83
jonas <jonas.grondahl@wexer.com>
2018-07-11 17:40:39 +0200

    some updates

--- src/App.js
+++ src/App.js
@@ -4,13 +4,40 @@ import axios from "axios";
 import "./App.css";
 import "./Calendar.css";
 import "fullcalendar/dist/fullcalendar.min.css";
-const FullCalendar = require("fullcalendar");
+import { Calendar } from "fullcalendar";
+import {
+  Drawer,
+  List,
+  ListItem,
+  Divider,
+  IconButton,
+  Toolbar,
+  AppBar,
+  Tooltip,
+  TextField,
+  FormControl,
+  Grid,
+  Select,
+  MenuItem,
+  InputLabel
+} from "@material-ui/core";
+import { ChevronRight, Layers, Search, Cloud } from "@material-ui/icons";
+import IconMenu from "@material-ui/icons/Menu";
+import SelectTypeDialog from "./components/SelectTypeDialog/SelectTypeDialog";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.refCalendar = React.createRef();
     this.calendar = null;
+    this.state = {
+      isDrawingOpen: true,
+      isTypeDialogOpen: true,
+      levels: ["None", "Beginner", "Advanced", "For everyone", "Intermediate"],
+      filters: {
+        level: "None"
+      }
+    };
   }
 
   componentDidMount() {
@@ -22,12 +49,11 @@ class App extends Component {
       editable: true,
       header: false,
       columnFormat: "dddd",
-      firstDay: 1
+      firstDay: 1,
+      slotDuration: "00:05:00",
+      height: "parent"
     };
-    this.calendar = new FullCalendar.Calendar(
-      this.refCalendar.current,
-      options
-    );
+    this.calendar = new Calendar(this.refCalendar.current, options);
     this.calendar.render();
   }
 
@@ -39,16 +65,17 @@ class App extends Component {
       `https://api-wexer.herokuapp.com/v1/calendars/7364?start=${startdate}&end=${enddate}`
     );
     const mappedData = response.data.result.map(event => {
-      console.log(event);
       let color = "";
       switch (event.sf_kategori) {
         case "Cycling (Cycling)":
-          color = "#0091EA";
+          color = "var(--eventCycling)";
+          break;
+        case "D´stress and relax (Mind-body)":
+          color = "var(--eventColor)";
           break;
         default:
-          color = "#ff9800";
+          color = "var(--eventCycling)";
       }
-      console.log(color);
       return {
         ...event,
         start: event.startdate,
@@ -59,16 +86,107 @@ class App extends Component {
     callback(mappedData);
   };
 
+  selectType = type => {
+    this.setState({ type, isTypeDialogOpen: false });
+  };
+
+  onLevelChange = event => {
+    const { filters } = this.state;
+    filters.level = event.target.value;
+    this.setState({ filters });
+  };
+
   render() {
+    const { isDrawingOpen, isTypeDialogOpen, levels, filters } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <br />
-        <div className="calendar-container">
+        <div
+          className="calendar-container"
+          style={{ marginLeft: isDrawingOpen ? "var(--calendarGutter)" : 30 }}
+        >
+          <AppBar
+            position="static"
+            style={{ boxShadow: "none", marginBottom: 5 }}
+          >
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                onClick={() =>
+                  this.setState({ isDrawingOpen: !this.state.isDrawingOpen })
+                }
+              >
+                <IconMenu />
+              </IconButton>
+              <div style={{ marginLeft: "auto" }}>
+                <Tooltip
+                  title="Toggle simple/advanced view"
+                  style={{ fontSize: 16 }}
+                >
+                  <IconButton
+                    color="inherit"
+                    onClick={() => this.setState({ isTypeDialogOpen: true })}
+                  >
+                    <Layers />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Publish changes to the Wexer player">
+                  <IconButton
+                    color="secondary"
+                    onClick={() => this.setState({ isPublishDialogOpen: true })}
+                  >
+                    <Cloud />
+                  </IconButton>
+                </Tooltip>
+              </div>
+            </Toolbar>
+          </AppBar>
           <div id="calendar" ref={this.refCalendar} />
         </div>
+        <Drawer variant="persistent" anchor="left" open={isDrawingOpen}>
+          <div className="content">
+            <IconButton
+              onClick={() =>
+                this.setState({ isDrawingOpen: !this.state.isDrawingOpen })
+              }
+            >
+              <ChevronRight />
+            </IconButton>
+            <Divider />
+            <List>
+              <ListItem>
+                <Grid container spacing={8} alignItems="flex-end">
+                  <Grid item>
+                    <Search />
+                  </Grid>
+                  <Grid item>
+                    <TextField label="Search" />
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <FormControl fullWidth>
+                  <InputLabel>Filter by Level</InputLabel>
+                  <Select
+                    onChange={this.onLevelChange}
+                    value={filters.level}
+                    label="Filter by level"
+                  >
+                    {levels.map(level => (
+                      <MenuItem value={level}>{level}</MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </ListItem>
+              <ListItem>Lorem ipsum dolor sit amet.</ListItem>
+              <ListItem>Lorem ipsum dolor sit amet.</ListItem>
+            </List>
+          </div>
+          <SelectTypeDialog
+            show={isTypeDialogOpen}
+            selectType={this.selectType}
+          />
+        </Drawer>
       </div>
     );
   }
---
+++ src/components/SelectTypeDialog/SelectTypeDialog.js
@@ -0,0 +1,90 @@
+import React, { PureComponent, Fragment } from "react";
+import {
+  Dialog,
+  DialogContent,
+  Card,
+  CardMedia,
+  CardContent,
+  withStyles,
+  Modal,
+  CardActions,
+  Button
+} from "@material-ui/core";
+import "./SelectTypeDialog.css";
+
+const styles = {
+  root: {
+    margin: 5
+  }
+};
+
+class SelectTypeDialog extends PureComponent {
+  componentDidMount() {
+    console.log(this.props);
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Dialog
+        BackdropProps={{ invisible: false }}
+        open={this.props.show}
+        className="select-type-dialog"
+        disableRestoreFocus
+      >
+        <Card classes={{ root: classes.root }}>
+          <CardMedia
+            image="https://drm-wizard.com/wp-content/uploads/2015/04/easy-mode.png"
+            style={{
+              height: 150
+            }}
+          />
+          <CardContent>
+            <h1>Simple</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+              accusantium ducimus delectus dolore earum perspiciatis recusandae
+              nobis numquam beatae amet!
+            </p>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => this.props.selectType("simple")}
+            >
+              Select simple
+            </Button>
+          </CardActions>
+        </Card>
+        <Card classes={{ root: classes.root }}>
+          <CardMedia
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Albert_Einstein_Head.jpg/220px-Albert_Einstein_Head.jpg"
+            style={{
+              height: 150
+            }}
+          />
+          <CardContent>
+            <h1>Advanced</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+              corrupti quas nemo soluta consequuntur accusantium dolores
+              consectetur, voluptates voluptas harum.
+            </p>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={() => this.props.selectType("advanced")}
+            >
+              Select advanced
+            </Button>
+          </CardActions>
+        </Card>
+      </Dialog>
+    );
+  }
+}
+
+export default withStyles(styles)(SelectTypeDialog);
