61fb0a73ae944da65fc36ca200653c2e144c8cc3
jonas <jonas.grondahl@wexer.com>
2018-07-16 13:16:06 +0200

    changes

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001"
+  baseURL: (process.env.NODE_ENV = "development"
+    ? "http://localhost:3001"
+    : "https://api-wexer.herokuapp.com")
 });
 
 export default axiosInstance;
