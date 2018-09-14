615992b3b3ea923e58baf41b431e064724f32977
jonasgroendahl <jonas.grondahl@wexer.com>
2018-09-14 15:25:02 +0200

    NODE_ENV changed back to work for production

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    process.env.NODE_ENV === "production"
+    process.env.NODE_ENV === "development"
       ? "http://localhost:3001"
       : "https://api-wexer.herokuapp.com"
 });
