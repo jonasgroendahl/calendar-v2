06d16c5b708d8b6e9a736637ffab9e5a42b5bcd6
jonas <jonas.grondahl@wexer.com>
2018-11-27 15:03:07 +0100

    new flag cus firefox -.-

--- src/components/Flag/Flag.js
+++ src/components/Flag/Flag.js
@@ -23,12 +23,10 @@ export default function Flag(props) {
       break;
     case 'Japanese':
       svg = <div className="flag-svg" dangerouslySetInnerHTML={{
-        __html: `<svg xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-jp" viewBox="0 0 512 512"><defs><clipPath id="a"><path fill-opacity=".7" d="M177.2 0h708.6v708.7H177.2z"/></clipPath>
-      </defs><g fill-rule="evenodd" stroke-width="1pt" clip-path="url(#a)" transform="translate(-128) scale(.72249)">
-      <path fill="#fff" d="M0 0h1063v708.7H0z"/>
-      <ellipse cx="523.1" cy="344.1" fill="#d30000" rx="194.9" ry="194.9" transform="translate(-59.7 -34.5) scale(1.1302)"/>
-      </g>
-    </svg>`}} />
+        __html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 600">
+        <rect fill="#fff" height="600" width="900"/>
+        <circle fill="#bc002d" cx="450" cy="300" r="180"/>
+        </svg>`}} />
       break;
     case 'German':
       svg = <div className="flag-svg" dangerouslySetInnerHTML={{
